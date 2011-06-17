d97092cf009dfd3f5868b23bf90fe68d2cd2772b
Markus Fisch <mf@markusfisch.de>
2011-06-17 19:20:40 +0200

    updated version number

--- src/main.cpp
+++ src/main.cpp
@@ -84,7 +84,7 @@ int main( int argc, char **argv )
 							return 0;
 						case 'v':
 							std::cout <<
-								binary << " 1.3.0" <<
+								binary << " 1.3.1" <<
 								std::endl <<
 								"Copyright (c) 2007-2011" <<
 								std::endl <<
