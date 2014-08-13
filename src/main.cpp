11d4bc6efa3b33aa4a90073fd02db19164e7305f
Markus Fisch <mf@markusfisch.de>
2014-08-13 22:29:26 +0200

    Advanced version number

--- src/main.cpp
+++ src/main.cpp
@@ -94,7 +94,7 @@ int main( int argc, char **argv )
 							return 0;
 						case 'v':
 							std::cout <<
-								binary << " 1.6.5" <<
+								binary << " 1.6.6" <<
 								std::endl <<
 								"Copyright (c) 2007-2014" <<
 								std::endl <<
